4fd3efd46c60c38a9d2a6ba6e2b8b8ce41b6395a
Miso Zmiric (Mike Squinter) <miso.zmiric@gmail.com>
2017-07-28 10:45:14 +0200

    updates the runner to use fork in order to launch up a new node instance
    when we launch a new node instance we fire up the run helper, which
    should then run a dynamic import on the script matching the job and
    call a function on it in order to have the job executed

---
+++ scripts/runHelper.js
@@ -0,0 +1,6 @@
+const jobArgs = process.argv.slice(2);
+console.log(jobArgs);
+
+//TODO: do a dynamic require based on the name of the script we want to run
+//and run call on it in order to pass down the array of the arguments from the
+//job
